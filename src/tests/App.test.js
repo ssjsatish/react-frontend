34b8aadac549ff750569880c350b16acd694275a
Satish Patel <SSJ.SATISH@GMAIL.COM>
2021-06-01 15:47:52 +0530

    testing travis ci

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('testing renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
